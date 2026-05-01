c87932d9ff972f759c24fe2cfdbb43ba16fee367
patriciaortizc <patriciaortizcobos@gmail.com>
2026-05-01 23:34:01 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.querySelector('.menu');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        menu.classList.toggle('open');
+    });
